d395b7a82eb382663f0965bf3e87602dc5224678
usman-tahir <tahir.usman.ali94@outlook.com>
2017-06-01 19:57:41 -0400

    added compiled app files

---
+++ hello-world/app.compiled.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var React = require('react'),
+    ReactDOM = require('react-dom');
+
+ReactDOM.render(React.createElement('h1', null, 'Hello World'), document.getElementById("app"));
